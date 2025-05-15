5fbe99ddd98e044cf3a15e578296c44f8d38ba1f
Victor <symone.villaver35@gmail.com>
2025-05-15 22:37:25 +0800

    Projects v2

--- projects.php
+++ projects.php
@@ -16,9 +16,48 @@
         <main>
             <?php include("php/aside.php"); ?>
             <section id="mainContent">
-                <h2>OUR PROJECTS</h2>
-                <p>LIST OF PROJECTS</p>
-                <div></div>
+                <a class="project">
+                    <img src="img/png/homeCarou1.png" alt="Image 1">
+                    <div class="projectText">
+                        <h2>Project Title 1</h2>
+                        <p>Description of the project goes here. This is a brief overview of what the project is about.</p>
+                    </div>
+                </a>
+                <a class="project">
+                    <img src="img/png/homeCarou2.png" alt="Image 2">
+                    <div class="projectText">
+                        <h2>Project Title 2</h2>
+                        <p>Description of the project goes here. This is a brief overview of what the project is about.</p>
+                    </div>
+                </a>
+                <a class="project">
+                    <img src="img/png/homeCarou3.png" alt="Image 3">
+                    <div class="projectText">
+                        <h2>Project Title 3</h2>
+                        <p>Description of the project goes here. This is a brief overview of what the project is about.</p>
+                    </div>
+                </a>
+                <a class="project">
+                    <img src="img/png/homeCarou4.png" alt="Image 4">
+                    <div class="projectText">
+                        <h2>Project Title 4</h2>
+                        <p>Description of the project goes here. This is a brief overview of what the project is about.</p>
+                    </div>
+                </a>
+                <a class="project">
+                    <img src="img/png/homeCarou5.png" alt="Image 5">
+                    <div class="projectText">
+                        <h2>Project Title 5</h2>
+                        <p>Description of the project goes here. This is a brief overview of what the project is about.</p>
+                    </div>
+                </a>
+                <a class="project">
+                    <img src="img/png/homeCarou1.png" alt="Image 6">
+                    <div class="projectText">
+                        <h2>Project Title 6</h2>
+                        <p>Description of the project goes here. This is a brief overview of what the project is about.</p>
+                    </div>
+                </a>
             </section>
         </main>
         <?php include("php/footer.php"); ?>
